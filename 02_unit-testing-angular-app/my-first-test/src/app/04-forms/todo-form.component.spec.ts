d2ec74db8d48aa37a3371b93a05f93450191b27f
valeriorisuleo <valerio.risuleo@gmail.com>
2019-05-15 23:50:13 +0100

    upvoted component test done

--- 02_unit-testing-angular-app/my-first-test/src/app/04-forms/todo-form.component.spec.ts
+++ 02_unit-testing-angular-app/my-first-test/src/app/04-forms/todo-form.component.spec.ts
@@ -1,23 +1,25 @@
-import { TodoFormComponent } from './todo-form.component';
-import { FormBuilder } from '@angular/forms';
-
-describe('TodoFormComponent', () => {
-  var component: TodoFormComponent;
-
-  beforeEach(() => {
-    component = new TodoFormComponent(new FormBuilder());
-  });
-
-  it('should create  form with 2 controls', () => {
-    expect(component.form.contains('name')).toBeTruthy();
-    expect(component.form.contains('email')).toBeTruthy();
-  });
-
-  it('should make the name control required', () => {
-    let control = component.form.get('name');
-
-    control.setValue('');
-
-    expect(control.valid).toBeFalsy();
-  });
-});
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+//
+// import { TodoFormComponent } from './todo-form.component';
+//
+// describe('TodoFormComponent', () => {
+//   let component: TodoFormComponent;
+//   let fixture: ComponentFixture<TodoFormComponent>;
+//
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ TodoFormComponent ]
+//     })
+//     // .compileComponents();
+//   }));
+//
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(TodoFormComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
